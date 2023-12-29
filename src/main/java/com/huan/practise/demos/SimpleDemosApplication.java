319d6b3c71d2fab396e2b0a9127f379b9ba35b4c
huan <leojinx624@163.com>
2023-12-29 22:50:20 +0800

    初始化;

---
+++ src/main/java/com/huan/practise/demos/SimpleDemosApplication.java
@@ -0,0 +1,13 @@
+package com.huan.practise.demos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDemosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleDemosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huan/practise/demos/SimpleDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huan.practise.demos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDemosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
